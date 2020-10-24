79e58ba78f424eb318ae738c1bd400f4e07a5a16
Derek Zumsteg <milhousian@users.noreply.github.com>
2020-10-24 13:26:36 -0700

    did I change something?

--- p5/2020-10-22 towards a grid/Critters.js
+++ p5/2020-10-22 towards a grid/Critters.js
@@ -28,22 +28,21 @@ function Critter(){
 
 // this depends on it being passed the position *in critter array* of the neighbors
 	this.updateColors = function(prev_neighbor, next_neighbor){
-		print("trying to update colors");
 		let temp_prev_neighbor = prev_neighbor;
 		let temp_next_neighbor = next_neighbor;
 		let temp_prev_neighbor_color = critterArray[temp_prev_neighbor].displayed_color;
 		let temp_next_neighbor_color = critterArray[temp_next_neighbor].displayed_color;
 		let temp_color_holder = 0;
-		print("was passed", temp_prev_neighbor, temp_next_neighbor)
-		print("I think their colors are ", temp_prev_neighbor_color, temp_next_neighbor_color);
+// 		print("was passed", temp_prev_neighbor, temp_next_neighbor)
+// 		print("I think their colors are ", temp_prev_neighbor_color, temp_next_neighbor_color);
 		temp_color_holder = round((this.displayed_color + this.natural_color + temp_prev_neighbor_color + temp_next_neighbor_color)/4)
 		let desired_change = abs(temp_color_holder-this.displayed_color); // how far is the distance
 		if ((desired_change * food_cost_to_move) < this.food){   // only change if it has enough energy to get there
 			this.food = this.food - (desired_change  * food_cost_to_move);
 			this.displayed_color = temp_color_holder;
+			print("someone moved!");
 		} 
 	}	
-		
 		
 	this.checkForExpansionReady = function(){
 		// conservative initially, has to able to survive next frame at new size w/o food

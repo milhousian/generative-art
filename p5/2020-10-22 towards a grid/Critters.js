818903f9c2f4dbce0fc8a515c564a5499d28a14f
Derek Zumsteg <milhousian@users.noreply.github.com>
2020-10-25 23:20:14 -0700

    everything is busted
    attempting to implement expansion into empty cells has led to a nightmare in which undefined and nothing makes sense

--- p5/2020-10-22 towards a grid/Critters.js
+++ p5/2020-10-22 towards a grid/Critters.js
@@ -1,11 +1,11 @@
 function Critter(){
 	this.natural_color = round(random(0,255));
-	this.displayed_color = 0;
+	this.displayed_color = this.natural_color;
 	this.am_alive = 1;
 	this.health = 50;
 	this.food = 1;
-	this.towards_natural = 0;
-	this.towards_neighbors = 0;
+	this.towards_natural = 0;  // not implemented
+	this.towards_neighbors = 0; // not implemented
 	this.size = 1;
 	this.age = 0;
 	this.ready_to_expand = 0;
@@ -13,9 +13,10 @@ function Critter(){
 
 	this.timePasses = function(){
 		this.age++;
-		let death = random(0,2000);
-		if (death<1){
+		let death = random(0,4);
+		if (death<2){
 			this.die();
+			print("died of random");
 		}
 	}
 	
@@ -25,15 +26,26 @@ function Critter(){
 		this.size = 0;	
 	}
 	
+	this.eatDinner = function(){
+		this.food = this.food - (this.size^2 * food_cost_basic);
+		if (this.food<0){
+			this.die;
+			print("oof starved to death");
+		}
+		if((this.size*food_cost_basic )< this.food ){
+			this.ready_to_expand = 1;
+		}
+	}
 
 // this depends on it being passed the position *in critter array* of the neighbors
 	this.updateColors = function(prev_neighbor, next_neighbor){
+// 		print("was passed", prev_neighbor, next_neighbor)
 		let temp_prev_neighbor = prev_neighbor;
 		let temp_next_neighbor = next_neighbor;
 		let temp_prev_neighbor_color = critterArray[temp_prev_neighbor].displayed_color;
 		let temp_next_neighbor_color = critterArray[temp_next_neighbor].displayed_color;
 		let temp_color_holder = 0;
-// 		print("was passed", temp_prev_neighbor, temp_next_neighbor)
+// 		
 // 		print("I think their colors are ", temp_prev_neighbor_color, temp_next_neighbor_color);
 		temp_color_holder = round((this.displayed_color + this.natural_color + temp_prev_neighbor_color + temp_next_neighbor_color)/4)
 		let desired_change = abs(temp_color_holder-this.displayed_color); // how far is the distance
@@ -45,11 +57,12 @@ function Critter(){
 	}	
 		
 	this.checkForExpansionReady = function(){
-		// conservative initially, has to able to survive next frame at new size w/o food
-		if((this.size*food_cost_basic )< this.food ){
-			this.ready_to_expand = 1;
-			print("ready to go");
-		}
+// currently done in dinner eating
+// 		conservative initially, has to able to survive next frame at new size w/o food
+// 		if((this.size*food_cost_basic )< this.food ){
+// 			this.ready_to_expand = 1;
+// 			print("ready to go");
+// 		}
 	}
 	
 	
---
+++ p5/make-p5-book/chapter-13/sketch.js
@@ -0,0 +1,15 @@
+var capture;
+
+function setup(){
+ 	createCanvas(480,240);
+	capture = createCapture();
+//	capture.hide();
+}
+
+function draw(){
+	var aspectRatio = capture.height/capture.width;
+	var h = width * aspectRatio;
+	image(capture, 0, 0, width, h);
+	filter(INVERT);
+}
+	

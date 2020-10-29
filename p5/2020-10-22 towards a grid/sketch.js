3d4e18becdb09973c89086a6808ed4fd4ce3c13b
Nicole Tibaldi <ntibaldi@gmail.com>
2020-10-28 20:34:50 -0400

    first pass at a refactor, v rough

--- p5/2020-10-22 towards a grid/sketch.js
+++ p5/2020-10-22 towards a grid/sketch.js
@@ -5,11 +5,10 @@
 var height = 400;
 var width = 400;
 var target_framerate = 10;
-var number_rows = 4;
+var number_rows = 40;
 var row_height = 0; // we'll calculate this in a second in setup()
 var rowOccupants = []; // array with number of rows, with the position of occupying critter
 var critterArray = [];
-var emptyCellLocations = [];
 var uniqueRowOccupants = [];
 var global_food_to_allocate = 3 * number_rows; // how much do you want each row to get to munch
 var food_available_to_distribute = 0;
@@ -37,11 +36,10 @@ function draw() {
   ageCritters(); // can kill off cells
   allocateFood();
   dinnerTime(); // can kill off cells
-  catalogEmptyCells();
   fillEmptyCellsWithExpansionThenNew();
   catalogUniqueRowOccupants(); // the order in which cells die and are catalogued matters a lot
   everybodyChangeColors(); // dependent on that catalog
-  print("frame complete");
+  // print("frame complete");
   // debugger;   // uncomment to debug per-frame
 }
 
@@ -51,7 +49,7 @@ function draw() {
 
 // ah, drawRows. Good old trusty drawRows. Remember when this was the troublesome function? those were the days.
 function drawRows() {
-  for (var i = 0; i < number_rows; i += 1) {
+  for (var i = 0; i < number_rows; i++) {
     rectMode(CENTER);
     let centerx = width / 2;
     let centery = row_height / 2;
@@ -92,17 +90,16 @@ function drawRows() {
 
 function determineFoodAvailable() {
   // this is where we could look at what the colors and whatever are and make a calculation
-  food_available_to_distribute =
-    food_available_to_distribute + global_food_to_allocate; // set this to zero after
+  food_available_to_distribute += global_food_to_allocate; // set this to zero after
 }
 
 // then let's go through all the critters and tell them time marches on
 function ageCritters() {
-  for (var i = 0; i < critterArray.length; i++) {
-    if (critterArray[i].am_alive == 1) {
-      critterArray[i].timePasses();
+  critterArray.forEach(critter => {
+    if (critter.am_alive === 1) {
+      critter.timePasses();
     }
-  }
+  })
 }
 
 // note that this doesn't check if they're alive or not, because who cares
@@ -111,95 +108,101 @@ function allocateFood() {
   let food_per_row = food_available_to_distribute / number_rows;
   // a couple ways we could do this -- I'm going to go with this very simple goldfish tank style one
   for (var i = 0; i < number_rows; i++) {
-    critterArray[rowOccupants[i]].food =
-      critterArray[rowOccupants[i]].food + food_per_row;
+    critterArray[rowOccupants[i]].food += food_per_row;
+    food_available_to_distribute = 0; // set this to zero
   }
-  food_available_to_distribute = 0; // set this to zero
 }
 
 function dinnerTime() {
-  for (var i = 0; i < uniqueRowOccupants.length; i++) {
-    if (critterArray[uniqueRowOccupants[i]].am_alive == 1) {
-      critterArray[uniqueRowOccupants[i]].eatDinner();
+  uniqueRowOccupants.forEach((occupant) => {
+    if (critterArray[occupant].am_alive == 1) {
+      critterArray[occupant].eatDinner();
       // print("called out to eat dinner");
     }
-  }
+  });
 }
 
 // go through and see where there are vacancies
 function catalogEmptyCells() {
-  emptyCellLocations = []; // gotta empty it out before we run each time
+  let emptyCells = [];
   for (var i = 0; i < number_rows; i++) {
     if (critterArray[rowOccupants[i]].am_alive == 0) {
       // append that location to the array of empty cell locations
-      emptyCellLocations.push(i);
+      emptyCells.push(i);
     }
   }
   // if they're all dead, let's just throw it into debugger
-  if (emptyCellLocations.length == number_rows) {
+  if (emptyCells.length == number_rows) {
     debugger;
   }
+  return emptyCells;
 }
 
 function fillEmptyCellsWithExpansionThenNew() {
   // we have an array of empty cell locations. for each of these, is the previous or next cell ready?
 
-  for (var i = 0; i < emptyCellLocations.length; i++) {
-    let current_vacancy_row = emptyCellLocations[i];
+  // NLT: I moved this inside here, I don't think this needs to be a global variable since it's only used here
+  // and this way you don't need to clear it each time
+  let emptyCellLocations = catalogEmptyCells();
+
+  emptyCellLocations.forEach((current_vacancy_row, i) => {
+    // NLT: why are these -1?
     let prev_row = -1;
     let next_row = -1;
     // define where to look for the next/previous neighbors
     // making sure we're not first or last, which would cause an out of bounds error
-    if (current_vacancy_row != 0 && current_vacancy_row != number_rows - 1) {
+    if (current_vacancy_row !== 0 && current_vacancy_row !== number_rows - 1) {
       // check the previous to see if it's ready_to_expand
       prev_row = current_vacancy_row - 1;
       next_row = current_vacancy_row + 1;
-    } else if (current_vacancy_row == 0) {
+    } else if (current_vacancy_row === 0) {
       prev_row = number_rows - 1;
       next_row = current_vacancy_row + 1;
-    } else if (current_vacancy_row == number_rows - 1) {
+    } else if (current_vacancy_row === number_rows - 1) {
       prev_row = current_vacancy_row - 1;
       next_row = 0;
     }
 
     // those defined we can now move to attempting a fill from them
     if (
-      critterArray[rowOccupants[prev_row]].am_alive == 1 &&
-      critterArray[rowOccupants[prev_row]].ready_to_expand == 1
+      critterArray[rowOccupants[prev_row]].am_alive === 1 &&
+      critterArray[rowOccupants[prev_row]].ready_to_expand === 1
     ) {
       fillEmptyCellFromNeighbor(emptyCellLocations[i], rowOccupants[prev_row]);
     } else if (
-      critterArray[rowOccupants[next_row]].am_alive == 1 &&
-      critterArray[rowOccupants[next_row]].ready_to_expand == 1
+      critterArray[rowOccupants[next_row]].am_alive === 1 &&
+      critterArray[rowOccupants[next_row]].ready_to_expand === 1
     ) {
       fillEmptyCellFromNeighbor(emptyCellLocations[i], rowOccupants[next_row]);
     } else {
       fillEmptyCellWithNew(emptyCellLocations[i]);
     }
-  }
+  });
 }
 
 // certainly no bugs in this function nosireeeeee
 function fillEmptyCellFromNeighbor(cellToFill, neighborCritterID) {
-  let tempCellToFill = cellToFill;
+  // let tempCellToFill = cellToFill;
+  // NLT: I don't see a need for the above variable (tell me I'm wrong!) so I think it's just obscuring stuff
   let parentCritter = neighborCritterID;
-  critterArray[parentCritter].size = critterArray[parentCritter].size++; // tell the creature it's bigger
-  critterArray[parentCritter].rows_occupied.push(tempCellToFill); // hopefully, add this to positions
-  rowOccupants[tempCellToFill] = parentCritter;
-  print(
-    "expanded from neighbor to fill ",
-    tempCellToFill,
-    " parent was ",
-    parentCritter
-  );
+  critterArray[parentCritter].size++; // tell the creature it's bigger
+  critterArray[parentCritter].rows_occupied.push(cellToFill); // hopefully, add this to positions
+  rowOccupants[cellToFill] = parentCritter;
+  // print(
+  //   "expanded from neighbor to fill ",
+  //   tempCellToFill,
+  //   " parent was ",
+  //   parentCritter
+  // );
 }
 
 function fillEmptyCellWithNew(cellToFill) {
-  let tempCellToFill = cellToFill;
+  // let tempCellToFill = cellToFill;
+  // NLT: same as above
   let tempCritter = new Critter(cellToFill); // generate a new Critter
   critterArray.push(tempCritter); // add it to the critter array
-  rowOccupants[tempCellToFill] = critterArray.length - 1; // go to the rowOccupant where we know there's a vacancy, put the id of the new one in
-  print("new organism created ");
+  rowOccupants[cellToFill] = critterArray.length - 1; // go to the rowOccupant where we know there's a vacancy, put the id of the new one in
+  // print("new organism created ");
 }
 
 function fancyCalc(hue_one, hue_two) {
@@ -208,12 +211,9 @@ function fancyCalc(hue_one, hue_two) {
   // you can't ever be more than 180 away from another point
   // so we're going to do some awful awful math
 
-  var delta = 0;
-  var abs_delta = 0;
-  var calcd_true_distance = 0;
-
-  delta = hue_one - hue_two;
-  abs_delta = abs(delta);
+  let delta = hue_one - hue_two;
+  let abs_delta = abs(delta);
+  let calcd_true_distance;
 
   if (abs_delta >= 180) {
     // 		print("abs delta was greater than 180")
@@ -235,10 +235,10 @@ function fancyCalc(hue_one, hue_two) {
 }
 
 function everybodyChangeColors() {
-  for (var i = 0; i < uniqueRowOccupants.length; i++) {
-    critterArray[[uniqueRowOccupants[i]]].updateNeighborInfo();
-    critterArray[[uniqueRowOccupants[i]]].updateColors();
-  }
+  uniqueRowOccupants.forEach((occupant) => {
+    critterArray[occupant].updateNeighborInfo();
+    critterArray[occupant].updateColors();
+  });
 }
 
 function catalogUniqueRowOccupants() {
@@ -254,15 +254,17 @@ function catalogUniqueRowOccupants() {
 }
 
 function createCritters() {
-  critterArray = [];
-  for (var i = 0; i < number_rows; i += 1) {
+  // critterArray = [];
+  //NLT: this function is only ever called once, and the array is already defined as an empty array so the above is... redundant?
+  for (var i = 0; i < number_rows; i++) {
     let newCritter = new Critter(i);
     critterArray.push(newCritter);
   }
 }
 
 function createRows() {
-  for (var i = 0; i < number_rows; i += 1) {
+  for (var i = 0; i < number_rows; i++) {
+    console.log("this was called");
     rowOccupants[i] = i; // each row starts out with the number of the critter occupying it
   }
 }
